8e5f704c25155081be38b50d4a3aa7a3b62a6f31
Coddyfish <treelord05@gmail.com>
2020-03-17 16:22:04 -0700

    Finished L5 mod 2

--- src/_02_Creating_Streams/CreatingStreams.java
+++ src/_02_Creating_Streams/CreatingStreams.java
@@ -2,31 +2,39 @@ package _02_Creating_Streams;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
 import java.util.stream.Stream;
 
 public class CreatingStreams {
 	public static void main(String[] args) {
-		//1. Create a stream out of the following collections.
-		String[] strArr = {"one", "two", "three", "four"};
-		
+		// 1. Create a stream out of the following collections.
+		String[] strArr = { "one", "two", "three", "four" };
+
+		Arrays.stream(strArr);
+		Stream<String> stream1 = Arrays.stream(strArr);
+
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
 		intList.add(2);
 		intList.add(3);
 		intList.add(4);
-		
+		Stream<Integer> stream2 = intList.stream();
+
 		Stack<Random> randStack = new Stack<Random>();
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
-		
+		Stream<Random> stream3 = randStack.stream();
+
 		ArrayDeque<Double> dQueue = new ArrayDeque<Double>();
 		dQueue.push(0.0);
 		dQueue.push(1.0);
 		dQueue.push(2.0);
 		dQueue.push(3.0);
+		Stream<Double> stream4 = dQueue.stream();
+
 	}
 }
--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -10,12 +10,17 @@ public class StreamSearcher {
 	// use Streams to help us out.
 	public long containsCount(String[] strArr, String strToCount) {
 		//2. Convert the String array to a stream
+		Stream<String> strS = Arrays.stream(strArr);
 		
 		//3. Use the filter method with a lambda to extract all the String
 		//   elements that match the String parameter.
+		strS = strS.filter(( s) ->{
+			return s.equals(strToCount);
+			
+		});
 		
 		//4. Return the count of the filtered stream.
 		
-		return 0;
+		return strS.count();
 	}
 }
